2bd6ec81cf39a69e936dc79f799f4b541853dadb
CamiloCortesM <camilo.cortes1753@gmail.com>
2024-01-18 11:51:04 -0500

    add colors styles and install icons svg

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,19 @@
 import { defineConfig } from 'astro/config';
-
 import tailwind from '@astrojs/tailwind';
+
+import icon from 'astro-icon';
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [
     tailwind({
       applyBaseStyles: false,
+    }),
+    icon({
+      include: {
+        tabler: ['*'],
+        ic:["*"],
+      },
     }),
   ],
 });
