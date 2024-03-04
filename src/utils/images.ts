06ba7c8722ff7f784fdc8c667665481c7beab366
CamiloCortesM <camilo.cortes1753@gmail.com>
2024-03-03 22:46:08 -0500

    Update image loading path to match the new directory structure

--- src/utils/images.ts
+++ src/utils/images.ts
@@ -1,14 +1,12 @@
 import { getImage } from 'astro:assets';
 import type { ImageMetadata } from 'astro';
 import type { OpenGraph } from '@astrolib/seo';
-import type { MetaDataImage } from '../types';
+import type { MetaDataImage } from '@/types';
 
 const load = async function () {
   let images: Record<string, () => Promise<unknown>> | undefined = undefined;
   try {
-    images = import.meta.glob(
-      '/images/*.{jpeg,jpg,png,tiff,webp,gif,svg,JPEG,JPG,PNG,TIFF,WEBP,GIF,SVG}'
-    );
+    images = import.meta.glob('../assets/images/**/*.{jpeg,jpg,png,tiff,webp,gif,svg,JPEG,JPG,PNG,TIFF,WEBP,GIF,SVG}');
   } catch (e) {
     // continue regardless of error
   }
