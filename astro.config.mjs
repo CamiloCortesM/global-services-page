d6c91b07d0052a5005783c36c9cf45dcca16f876
DANIELSSF <daniel.santiago730@gmail.com>
2024-03-11 12:34:29 -0500

    Fixed vercel controlator

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,20 +1,19 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
-
 import icon from 'astro-icon';
+
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   output: 'hybrid',
-  integrations: [
-    tailwind({
-      applyBaseStyles: false,
-    }),
-    icon({
-      include: {
-        tabler: ['*'],
-        ic:["*"], //TODO: optimize
-      },
-    }),
-  ],
+  integrations: [tailwind({
+    applyBaseStyles: false
+  }), icon({
+    include: {
+      tabler: ['*'],
+      ic: ["*"] //TODO: optimize
+    }
+  })],
+  adapter: vercel()
 });
