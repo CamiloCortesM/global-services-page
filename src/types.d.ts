4ba00016881bf238235bab4c5b15f2b6b51206dd
DANIELSSF <daniel.santiago730@gmail.com>
2024-03-11 21:48:09 -0500

    Add more options in type contact

--- src/types.d.ts
+++ src/types.d.ts
@@ -253,7 +253,11 @@ export interface Content extends Headline, Widget {
   callToAction?: CallToAction;
 }
 
-export interface Contact extends Headline, Form, Widget {}
+export interface Contact extends Headline, Form, Widget {
+  items: Array<Item>;
+  defaultIcon?: string;
+  columns: number;
+}
 
 export interface Quote {
   quote?: string;
