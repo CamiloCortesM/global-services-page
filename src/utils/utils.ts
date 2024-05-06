3b151d41914a9eca07cce0c16e68047861bb415d
CamiloCortesM <camilo.cortes1753@gmail.com>
2024-05-06 18:57:47 -0500

    Add function to remove special characters

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -5,3 +5,9 @@ export const trim = (str = '', ch?: string) => {
   while (end > start && str[end - 1] === ch) --end;
   return start > 0 || end < str.length ? str.substring(start, end) : str;
 };
+
+
+export const transformServiceName = (serviceName:string) => {
+  const transformedName = serviceName.replace(/[_-]/g, ' ');
+  return transformedName;
+}
