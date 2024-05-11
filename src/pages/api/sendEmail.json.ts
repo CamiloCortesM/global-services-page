2c74d69c0b464d43986cfc4a4de059ca00991842
CamiloCortesM <camilo.cortes1753@gmail.com>
2024-05-10 20:04:10 -0500

    Remove capchat verification

--- src/pages/api/sendEmail.json.ts
+++ src/pages/api/sendEmail.json.ts
@@ -2,63 +2,45 @@ import type { APIRoute } from 'astro';
 import { Resend } from 'resend';
 
 const resend = new Resend(import.meta.env.PUBLIC_RESEND_API_KEY);
-const SECRET_KEY = import.meta.env.PUBLIC_SECRET_KEY_TURNSTILE || '';
-const url = 'https://challenges.cloudflare.com/turnstile/v0/siteverify';
 
-export const POST: APIRoute = async ({ request,clientAddress }) => {
+export const POST: APIRoute = async ({ request }) => {
   const body = await request.json();
-  const { to, from, html, subject, text, cf_turnstile_response } = body;
+  const { to, from, html, subject, text } = body;
 
-  let formData = new FormData();
-
-  formData.append('secret', SECRET_KEY);
-  formData.append('response', cf_turnstile_response);
-  formData.append('remoteip', clientAddress);
-
-  const result = await fetch(url, {
-    body: formData,
-    method: 'POST',
-  });
-
-  const outcome = await result.json();
-  if (outcome.success) {
-    if (!to || !from || !html || !subject || !text) {
-      return new Response(null, {
-        status: 404,
-        statusText: 'Did not provide the right data',
-      });
-    }
-
-    const send = await resend.emails.send({
-      from,
-      to,
-      subject,
-      html,
-      text,
+  if (!to || !from || !html || !subject || !text) {
+    return new Response(null, {
+      status: 404,
+      statusText: 'Did not provide the right data',
     });
-
-    if (send.data) {
-      return new Response(
-        JSON.stringify({
-          message: send.data,
-        }),
-        {
-          status: 200,
-          statusText: 'OK',
-        }
-      );
-    } else {
-      return new Response(
-        JSON.stringify({
-          message: send.error,
-        }),
-        {
-          status: 500,
-          statusText: 'Internal Server Error',
-        }
-      );
-    }
   }
 
-  return new Response(null, { status: 403, statusText: 'Forbidden' });
+  const send = await resend.emails.send({
+    from,
+    to,
+    subject,
+    html,
+    text,
+  });
+
+  if (send.data) {
+    return new Response(
+      JSON.stringify({
+        message: send.data,
+      }),
+      {
+        status: 200,
+        statusText: 'OK',
+      }
+    );
+  } else {
+    return new Response(
+      JSON.stringify({
+        message: send.error,
+      }),
+      {
+        status: 500,
+        statusText: 'Internal Server Error',
+      }
+    );
+  }
 };
