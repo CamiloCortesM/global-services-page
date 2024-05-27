ff793b09f7f7217b8c1aafc69ab2994b61d4462b
CamiloCortesM <camilo.cortes1753@gmail.com>
2024-05-26 22:39:55 -0500

    Refactor response messages and remove duplicate code

--- src/pages/api/sendEmail.json.ts
+++ src/pages/api/sendEmail.json.ts
@@ -5,17 +5,27 @@ import { Resend } from 'resend';
 const resend = new Resend(import.meta.env.PUBLIC_RESEND_API_KEY);
 const email = import.meta.env.PUBLIC_EMAIL;
 
-export const POST: APIRoute = async ({ request }) => {
-  const body = await request.json();
-  const { html, subject, text } = body;
+const STATUS_OK = 200;
+const STATUS_BAD_REQUEST = 400;
+const STATUS_INTERNAL_SERVER_ERROR = 500;
 
-  if (!html || !subject || !text) {
-    return new Response(null, {
-      status: 404,
-      statusText: 'Did not provide the right data',
-    });
+const getMessage = (data: any, error: any) => {
+  if (data) {
+    return { message: data };
+  } else {
+    return { message: error };
   }
+};
 
+const sendEmail = async ({
+  html,
+  subject,
+  text,
+}: {
+  html: string;
+  subject: string;
+  text: string;
+}) => {
   const send = await resend.emails.send({
     from: 'Global Service & Maintenance <onboarding@resend.dev>',
     to: [email],
@@ -24,25 +34,27 @@ export const POST: APIRoute = async ({ request }) => {
     text,
   });
 
-  if (send.data) {
-    return new Response(
-      JSON.stringify({
-        message: send.data,
-      }),
-      {
-        status: 200,
-        statusText: 'OK',
-      }
-    );
-  } else {
-    return new Response(
-      JSON.stringify({
-        message: send.error,
-      }),
-      {
-        status: 500,
-        statusText: 'Internal Server Error',
-      }
-    );
+  return send;
+};
+
+export const POST: APIRoute = async ({ request }) => {
+  const body = await request.json();
+  const { html, subject, text } = body;
+
+  if (!html || !subject || !text) {
+    return new Response(null, {
+      status: STATUS_BAD_REQUEST,
+      statusText: 'Did not provide the right data',
+    });
   }
+
+  const send = await sendEmail({ html, subject, text });
+
+  return new Response(
+    JSON.stringify(getMessage(send.data, send.error)),
+    {
+      status: send.data ? STATUS_OK : STATUS_INTERNAL_SERVER_ERROR,
+      statusText: send.data ? 'OK' : 'Internal Server Error',
+    }
+  );
 };
